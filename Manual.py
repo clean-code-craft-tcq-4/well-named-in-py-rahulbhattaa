bf9b09e4f47f906b42560fa7280eeca9e23edcb5
rahulbhattaa <73284178+rahulbhattaa@users.noreply.github.com>
2022-07-01 10:53:29 +0530

    Create Manual.py

---
+++ Manual.py
@@ -0,0 +1,17 @@
+import colour_code
+
+
+def colour_codes_mapper():
+    colour_codes = []
+    pair_number = 1
+    for major_color in MAJOR_COLORS:
+        for minor_color in MINOR_COLORS:
+            color_codes.append(list((pair_number, major_color, minor_color)))
+            pair_number = pair_number + 1
+    return color_codes
+
+
+def print_manual():
+    colour_codes = colour_codes_mapper()
+    for colour_code in colour_codes:
+        print(color_code)
